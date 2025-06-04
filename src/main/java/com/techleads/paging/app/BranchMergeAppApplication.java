a3092864c4986a7c97d6b1facdb86305d3be1f5c
LAPTOP-5G07L8V1\anupo <anupoju.madhav@gmail.com>
2025-06-04 08:17:58 +0530

    add src code changes

---
+++ src/main/java/com/techleads/paging/app/BranchMergeAppApplication.java
@@ -0,0 +1,13 @@
+package com.techleads.paging.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BranchMergeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BranchMergeAppApplication.class, args);
+	}
+
+}
