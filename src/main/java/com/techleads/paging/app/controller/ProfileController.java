2ec13715ccb7aa943d9e15987a3fadfbc1f95cb0
LAPTOP-5G07L8V1\anupo <anupoju.madhav@gmail.com>
2025-06-04 09:36:55 +0530

    inject dependencies

--- src/main/java/com/techleads/paging/app/controller/ProfileController.java
+++ src/main/java/com/techleads/paging/app/controller/ProfileController.java
@@ -1,11 +1,19 @@
 package com.techleads.paging.app.controller;
 
+import com.techleads.paging.app.service.ProfileService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ProfileController {
+    private final ProfileService profileService;
+
+    public ProfileController(ProfileService profileService) {
+        this.profileService = profileService;
+    }
+
+
     @GetMapping("/health-check")
     public ResponseEntity<String> helthCheck() {
         return ResponseEntity.ok("Application is running");
--- src/main/java/com/techleads/paging/app/service/ProfileService.java
+++ src/main/java/com/techleads/paging/app/service/ProfileService.java
@@ -1,4 +1,12 @@
 package com.techleads.paging.app.service;
 
+import com.techleads.paging.app.repository.ProfileRepository;
+
 public class ProfileService {
+
+    private final ProfileRepository profileRepository;
+
+    public ProfileService(ProfileRepository profileRepository) {
+        this.profileRepository = profileRepository;
+    }
 }
